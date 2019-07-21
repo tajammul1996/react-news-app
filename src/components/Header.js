12cc86d173780e121552b996f4e926f0124e2449
tajammul1996 <tpzuffari@gmail.com>
2019-07-21 12:06:10 +0530

    added color to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div style={{ backgroundColor: "#D3D3D3", textAlign: "center" }}>
+      <h1 style={{ color: "#fff" }}>Header</h1>
     </div>
   );
 };
