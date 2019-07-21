6b129edaf1aca9bdd2433de8da958b67bcce3505
tajammul1996 <tpzuffari@gmail.com>
2019-07-21 15:15:15 +0530

    added news list item card

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Header from "./Header";
+import NewsListItem from "./NewsListitem";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <NewsListItem />
+      <NewsListItem />
+    </div>
+  );
 }
 
 export default App;
